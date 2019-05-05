5788302b956440546c035097ddb3299335bf1342
shengyushen <shengyushen@aliyun.com>
2019-05-05 15:23:19 +0000

    adding new script to install new tpu

---
+++ tpu_management/install_free_tpu.sh
@@ -0,0 +1,9 @@
+pip install tensorflow
+pip install mesh-tensorflow
+pip install tensorflow_datasets
+
+# installing t2t
+git clone https://github.com/tensorflow/tensor2tensor.git
+cd tensor2tensor/
+pip install --user  .
+
