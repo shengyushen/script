2d63667c49cf4e627f2a300ced2fd5046047a48b
shengyushen <shengyushen@gmail.com>
2019-05-05 23:35:03 +0800

    rearrage to use size as parameter of tpu

--- tpu_management/starttpu32.sh
+++
@@ -1,2 +0,0 @@
-ctpu up -name=ssy-mtf-ctpu -tf-version=1.13 -tpu-size=v2-32 -zone=us-central1-a --preemptible
-
---
+++ tpu_management/starttpugen.sh
@@ -0,0 +1,2 @@
+ctpu up -name=ssy-mtf-ctpu -tf-version=1.13 -tpu-size=v2-$1 -zone=us-central1-a --preemptible
+
