7388c69e25c66293b3705e5a29b598d253a22df4
U-CLOUDPC2C01\cloudpcqt08 <cloudpcqt08@CLOUDPC00A25494.CloudPC2C01.huawei.com>
2019-05-27 15:36:28 +0800

    adding new address

--- proxy/proxy.sh
+++ proxy/proxy.sh
@@ -1,3 +1,4 @@
-ssh -L 3128:localhost:8888 -i   /cygdrive/c/Users/cloudpcqt08/Downloads/japanproxy.pem.txt ubuntu@3.113.106.179
+ssh -L 3128:localhost:8888 -i   /cygdrive/c/Users/cloudpcqt08/Downloads/japanproxy.pem.txt ubuntu@176.34.17.205
+
 
 
