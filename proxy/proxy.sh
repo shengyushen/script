5d6201193e99381dd848b003cc1cebda1a292824
ssy <ssy@ssydeMacBook-Pro.local>
2019-05-28 12:20:42 +0800

    change addr

--- proxy/proxy.sh
+++ proxy/proxy.sh
@@ -1,5 +1,5 @@
 #ssh -L 3128:localhost:8888 -i   /cygdrive/c/Users/cloudpcqt08/Downloads/japanproxy.pem.txt ubuntu@176.34.17.205
-ssh -L 3128:localhost:8888 -i   ~/Downloads/japanproxy.pem.txt ubuntu@52.196.169.180
+ssh -L 3128:localhost:8888 -i   ~/Downloads/japanproxy.pem.txt ubuntu@3.113.122.225
 
 
 
