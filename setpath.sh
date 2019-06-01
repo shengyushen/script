27b4a9371e80d7898cb4f7e0c2b4c9d33dca7215
shengyushen <shengyushen@aliyun.com>
2019-06-01 21:41:12 +0800

    adding ~/script/ to path

--- setpath.sh
+++ setpath.sh
@@ -1,1 +1,1 @@
-export PATH=~/script/gitscr/:~/script/gs/:~/script/proxy/:~/script/rds/:~/script/tpu_management/:$PATH
+export PATH=~/script:~/script/gitscr/:~/script/gs/:~/script/proxy/:~/script/rds/:~/script/tpu_management/:$PATH
