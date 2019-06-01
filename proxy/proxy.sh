ef73b3c8321feb483f716ce6dddcdfad2d17dfa3
shengyushen <shengyushen@aliyun.com>
2019-06-01 21:57:42 +0800

    changing addess

--- proxy/proxy.sh
+++ proxy/proxy.sh
@@ -1,5 +1,5 @@
 #ssh -L 3128:localhost:8888 -i   /cygdrive/c/Users/cloudpcqt08/Downloads/japanproxy.pem.txt ubuntu@176.34.17.205
-ssh -L 3128:localhost:8888 -i   ~/Downloads/japanproxy.pem.txt ubuntu@52.68.55.219
+ssh -L 3128:localhost:8888 -i   ~/Downloads/japanproxy.pem.txt ubuntu@3.112.160.165
 
 
 
