161f93a494512a80cebb8b8ff5cddb18aa3316e6
U-CLOUDPC2C01\cloudpcqt08 <cloudpcqt08@CLOUDPC00A25494.CloudPC2C01.huawei.com>
2019-06-02 21:18:48 +0800

    share pem file

--- proxy/proxy.sh
+++ proxy/proxy.sh
@@ -1,4 +1,3 @@
-#ssh -L 3128:localhost:8888 -i   /cygdrive/c/Users/cloudpcqt08/Downloads/japanproxy.pem.txt ubuntu@176.34.17.205
 ssh -L 3128:localhost:8888 -i   ~/Downloads/japanproxy.pem.txt ubuntu@54.238.229.44
 
 
--- proxy/proxylinux.sh
+++
@@ -1,4 +0,0 @@
-ssh -L 3128:localhost:8888 -i   ~/Downloads/japanproxy.pem.txt ubuntu@176.34.17.205
-
-
-
