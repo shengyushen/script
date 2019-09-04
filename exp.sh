fdf94a0e6838f4b2e43d8ec374ea7dd2827a2536
shengyushen <shengyushen@aliyun.com>
2019-09-03 22:12:55 -0400

    adding exp.sh

--- exp.sh
+++ exp.sh
@@ -1,3 +1,3 @@
-export http_proxy="http://s00380291:mtsxdyt118484Huawei_@128.5.95.7:8080"
-export https_proxy="https://s00380291:mtsxdyt118484Huawei_@128.5.95.7:8080"
+export http_proxy="http://s00380291:mtsxdyt118484Huawei_@128.5.95.25:8080"
+export https_proxy="https://s00380291:mtsxdyt118484Huawei_@128.5.95.25:8080"
 
