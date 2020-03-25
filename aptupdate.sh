5229a3158f0c68dae5ad7d71b8d63068e40d498d
shengyushen <shengyushen@aliyun.com>
2020-03-25 04:44:41 -0400

    adding aptupdate

---
+++ aptupdate.sh
@@ -0,0 +1,2 @@
+apt-get update -o Acquire::https::developer.download.nvidia.com::Verify-Peer=false
+
--- tfversion.sh
+++ tfversion.sh
@@ -1,2 +1,2 @@
-python -c 'import tensorflow as tf; print(tf.__version__)'  # for Python 2
+#python -c 'import tensorflow as tf; print(tf.__version__)'  # for Python 2
 python3 -c 'import tensorflow as tf; print(tf.__version__)'  # for Python 3
